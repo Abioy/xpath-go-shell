c2c3b3b04c2c4f045bc58e9447399d81ed0ada1f
Yongyue Sun <abioy.sun@gmail.com>
2023-07-24 14:46:43 +0800

    Update xpath-go.go
    feat: XPath builtin functions

--- xpath-go.go
+++ xpath-go.go
@@ -3,12 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	//"launchpad.net/xmlpath"
-	xmlpath "gopkg.in/xmlpath.v2"
 	"os"
+
+	"github.com/antchfx/htmlquery"
+	"github.com/antchfx/xpath"
 )
 
-type Target map[string]*xmlpath.Path
+type TargetV2 map[string]*xpath.Expr
 
 func usage() string {
 	var help string
@@ -25,18 +26,11 @@ func usage() string {
 	return help
 }
 
-func Usage(errs ...string) {
-	rc := 0
-	var msg string
-	for _, err := range errs {
-		msg += "[ERROR] "
-		msg += err
-		msg += "\n"
-		rc = 1
-	}
-	msg += usage()
-	fmt.Fprint(os.Stderr, msg)
-	os.Exit(rc)
+func Usage(errs ...any) {
+	fmt.Fprint(os.Stderr, "[ERROR] ")
+	fmt.Fprint(os.Stderr, errs...)
+	fmt.Fprintf(os.Stderr, "\n%s", usage())
+	os.Exit(1)
 }
 
 func main() {
@@ -46,36 +40,48 @@ func main() {
 	}
 	pattern := args[1]
 	targets_str := args[2]
-	path, err := xmlpath.Compile(pattern)
+
+	var err error
+	// path, err := xmlpath.Compile(pattern)
+	expr, err := xpath.Compile(pattern)
 	if err != nil {
 		Usage("<PATH> illegal")
 	}
 	var data map[string]string
 	if json.Unmarshal([]byte(targets_str), &data) != nil {
-		Usage("<TARGET> illegal")
+		Usage("<TARGET> illegal json")
 	}
-	target := Target{}
+	target := TargetV2{}
 	for k, v := range data {
-		target[k], err = xmlpath.Compile(v)
+		target[k], err = xpath.Compile(v)
 		if err != nil {
-			Usage("<TARGET> path illegal")
+			Usage("<TARGET> path illegal: ", k, " => ", v)
 		}
 	}
 
 	file := os.Stdin
-	root, err := xmlpath.ParseHTML(file)
+	doc, err := htmlquery.Parse(file)
 	if err != nil {
 		Usage("parse os.Stdin failed")
 	}
 
-	it := path.Iter(root)
-	for it.Next() {
+	navigator := htmlquery.CreateXPathNavigator(doc)
+	t := expr.Select(navigator)
+	for t.MoveNext() {
+		curr := t.Current().Copy()
+		nav := curr.(*htmlquery.NodeNavigator)
 		result := map[string]string{}
 		for k, v := range target {
-			sub_it := v.Iter(it.Node())
 			result[k] = ""
-			if sub_it.Next() {
-				result[k] = sub_it.Node().String()
+			switch x := v.Evaluate(nav).(type) {
+			case bool, float64, string:
+				result[k] = fmt.Sprintf("%v", x)
+			case *xpath.NodeIterator:
+				if x.MoveNext() {
+					result[k] = x.Current().(*htmlquery.NodeNavigator).Value()
+				}
+			default:
+				panic(fmt.Sprintf("%t", x))
 			}
 		}
 		d, _ := json.Marshal(result)
